e3f675c5c937f79721795a96ae210e6d25f8e0e5
Isaac <iboates@gmail.com>
2017-05-18 17:43:58 +0200

    added weather card carousel template

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'slick-carousel';
+import 'slick-carousel/slick/slick';
 
 @Component({
   selector: 'app-carousel',
@@ -10,6 +13,13 @@ export class CarouselComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $('.weather-carousel').slick({
+      arrows: true,
+      slidesToShow: 5,
+      slidesToScroll: 1
+    });
+
   }
 
 }
--- src/control.js
+++ src/control.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-    
+
 $('#trig').on('click', function () {
     $('#form_area').toggleClass('col-0 col-xs-3');
     $('#map_area').toggleClass('col-xs-12 col-xs-9');
 });
+
 })
