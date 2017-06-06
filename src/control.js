fdbe84ee86eba0d06277408d3ed9dbd96ecb9be0
Adrián Castelló Martínez <adrianupvtopo@gmail.com>
2017-06-06 11:23:28 +0200

    changing

--- src/control.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    $('#toggle').on('click', function () {
-        $('#form_content').toggleClass('toggleForm');
-        $('#carousel').toggleClass("col-lg-12 col-lg-9 col-xs-12 col-xs-6");
-        
-    });
-
-})
